ada8d75ff07433554f004f287115ae3b10703cbf
loop1dev <92616640+loop1dev@users.noreply.github.com>
2025-03-04 22:16:13 -0600

    Update requests.ts
    typo

--- src/requests.ts
+++ src/requests.ts
@@ -61,6 +61,7 @@ export function createElasticInstance(
         auth: {
           username: elasticUser,
           password: elasticPassword
+        }
       })
     : new Client({
         node: elasticUrl,
